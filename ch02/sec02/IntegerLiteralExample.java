50af05dfe53622f14eb61fb9af99aa3e9b23d232
ELORA <dlwlals52@gmail.com>
2025-09-01 17:36:23 +0900

    0901 chapter02

---
+++ ch02/sec02/IntegerLiteralExample.java
@@ -0,0 +1,18 @@
+package ch02.sec02;
+
+public class IntegerLiteralExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 365;
+		int var4 = 0xB3;
+		
+		System.out.println("Var1" + var1);
+		System.out.println("Var2" + var2);
+		System.out.println("Var3" + var3);
+		System.out.println("Var4" + var4);
+	}
+
+}
